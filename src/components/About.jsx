2df70ba7a838ba00c506cc070b226dedb3f68756
sayalimore12 <sayalimore12@gmail.com>
2025-07-25 09:15:17 -0400

    porfolio content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,19 +25,24 @@ export default function About() {
        </Typography>
 
         <Typography variant="body1" paragraph>
-          My name is Sayali More, a Front-End UI/UX Developer with a strong foundation in building clean, scalable, and user-focused web applications. My journey in tech began after earning my Bachelor's in Computer Engineering, and since then, I’ve had the opportunity to work with multiple companies across various industries.
+         I'm a creative and detail-oriented Front-End Developer with over 7 years of experience building modern, responsive, and user-focused web applications. My expertise spans ReactJS, JavaScript, Material UI, Tailwind CSS, and CMS platforms like WordPress and Strapi.
+          I collaborate with design tools like Figma to create clean, reusable components and optimize UX and performance using Agile practices and Lighthouse audits.
+          I specialize in creating accessible, SEO-friendly websites by following WCAG standards and modern best practices, ensuring every digital experience is both inclusive and high-performing.
+        </Typography>
+        <Typography variant="body1" paragraph>
+            I'm passionate about bringing impactful web experiences to life. With a strong foundation in front-end development, modern tools, and user-centered design, I'm always excited to take on new projects. I aim to collaborate with clients and stakeholders to build accessible, responsive, and visually engaging websites that align with their goals and connect with their audiences.                 
         </Typography>
 
         <Typography variant="body1" paragraph>
-          One of the most defining moments in my career was being selected to represent my company for an international client project in the Netherlands. There, I led training sessions on our in-house ERP software and contributed to a successful deployment—an achievement that earned me the Star Performer Award.
+          One of the most defining moments in my career was being selected to represent my company for an international client project in the Netherlands. There, I led training sessions on our in-house ERP software and contributed to a successful deployment - an achievement that earned me the Star Performer Award.
         </Typography>
 
         <Typography variant="body1" paragraph>
-          Driven by a passion to deepen my technical expertise, I pursued a Master’s degree in Computer Science at Wayne State University, graduating with a GPA of 3.93. Currently, I’m working at the university, developing and maintaining a mentoring platform for the School of Business—combining my skills in frontend development, UI/UX, and problem-solving to enhance student engagement.
+          Driven by a passion to deepen my technical expertise, I pursued a Master's degree in Computer Science at Wayne State University, graduating in 2024 with a GPA of 3.93. Since then, I have focused on applying my front-end development and UI/UX design skills to deliver innovative, user-centered, and accessible web solutions that create lasting impact.
         </Typography>
 
         <Typography variant="body1" paragraph>
-          Beyond the screen, I’m an explorer at heart. I love traveling, camping under the stars, and experimenting with new recipes in the kitchen.
+          Beyond the screen, I'm an explorer at heart. I love traveling, camping under the stars, and experimenting with new recipes in the kitchen.
         </Typography>
       </Container>
     </Box>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,26 +7,22 @@ import '../styles/styles.scss';
 
 
 export default function Home() {
-    return (
-        <Box sx={{ pt: { xs: 8, md: 12 }, bgcolor: '#ff9b9b36', color: '#000000' }}>
-            <Container className="profile-section">
-                <div className="profile-flex">
-                    <div className="myPhotoContainer">
-                        <img src={myPhoto} alt="Sayali More" />
-                    </div>
-                    <div className="profile-text">
-                      <h1>Hi, I'm Sayali More</h1>
-                     <h2> Frontend Developer & UI/UX Specialist</h2> <i>Transforming ideas into engaging, user-focused digital solutions</i>
-                        <p className="introText">
-                            I design and build responsive, accessible, and user-focused digital experiences. With 7+ years of expertise in JavaScript, React, CMS platforms, and modern UI/UX design, I bring creativity and precision to every project. I follow WCAG accessibility standards to ensure inclusivity and leverage SEO best practices to create high-performing, discoverable websites
-                            <br /><br />
-                           I'm passionate about bringing impactful web experiences to life. With a strong foundation in front-end development, modern tools, and user-centered design, I'm always excited to take on new projects. I aim to collaborate with clients and stakeholders to build accessible, responsive, and visually engaging websites that align with their goals and connect with their audiences.
-                            <br /><br />
-                           
-                        </p>
-                    </div>
-                </div>
-            </Container>
-        </Box>
-    );
+  return (
+    <Box sx={{ pt: { xs: 8, md: 12 }, bgcolor: '#ff9b9b36', color: '#000000' }}>
+      <Container className="profile-section">
+        <div className="profile-flex">
+          <div className="myPhotoContainer">
+            <img src={myPhoto} alt="Sayali More" />
+          </div>
+          <div className="profile-text">
+            <h1>Hi, I'm Sayali More</h1>
+            <h2> Frontend Developer & UI/UX Specialist</h2> <i>Transforming ideas into engaging, user-focused digital solutions</i>
+            <p className="introText">
+              I design and build responsive, accessible, and user-focused digital experiences. With 7+ years of expertise in JavaScript, React, CMS platforms, and modern UI/UX design, I bring creativity and precision to every project. I follow WCAG accessibility standards to ensure inclusivity and leverage SEO best practices to create high-performing, discoverable websites.
+            </p>
+          </div>
+        </div>
+      </Container>
+    </Box>
+  );
 }
